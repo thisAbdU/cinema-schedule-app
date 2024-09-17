185d3235d91e3857422ad000349991ea1548c617
Abdu <abdulfetaheishak@gmail.com>
2024-09-18 02:32:16 +0300

    set up project

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-prettier',
+  ],
+  rules: {
+    "block-no-empty": true,
+  },
+}
