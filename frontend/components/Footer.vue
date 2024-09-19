7abaff6c146b41ea61fbaa454e00bc5cfd68700b
Abdu <abdulfetaheishak@gmail.com>
2024-09-19 18:37:23 +0300

    add footer with logo

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,64 +1,65 @@
 <template>
-    <footer class="bg-white dark:bg-gray-900">
+  <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div class="md:flex md:justify-between">
-            <div class="mb-6 md:mb-0">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="../assets/img/logo-no-background.svg" class="h-16" alt="Cinema Logo">
-                </a>
-            </div>
-            <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                    <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                        <li class="mb-4">
-                            <a href="https://www.instagram.com/yourprofile" class="hover:underline">Instagram</a>
-                        </li>
-                        <li>
-                            <a href="https://www.tiktok.com/@yourprofile" class="hover:underline">Tiktok</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                    <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+      <div class="md:flex md:justify-between">
+        <div class="mb-6 md:mb-0">
+          <a href="https://yourwebsite.com/" class="flex items-center">
+            <img src="@/assets/img/logo-no-background.svg" class="h-16" alt="Your Company Logo" />
+          </a>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-            <div class="flex mt-4 sm:justify-center sm:mt-0 space-x-4">
-                <a href="https://www.facebook.com/yourprofile" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                        <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-                    </svg>
-                    <span class="sr-only">Facebook page</span>
-                </a>
-
-                <a href="https://www.tiktok.com/@yourprofile" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M14.81 2.41a4.936 4.936 0 0 0-1.886-.396c-2.718 0-4.929 2.217-4.929 4.95v4.93c0 2.73 2.21 4.948 4.929 4.948 1.043 0 2.053-.312 2.866-.845a1.888 1.888 0 0 0 1.12-.962c.49-1.099.607-2.244.339-3.368-.31-1.233-1.14-2.32-2.256-2.717-.391-.117-.78-.18-1.176-.18-.652 0-1.318.195-1.846.549-.843.568-1.579 1.365-1.983 2.244-.512 1.016-.664 2.152-.367 3.231.093.274.303.528.569.692a1.823 1.823 0 0 0 .887.29 1.897 1.897 0 0 0 1.858-1.84v-5.33a1.897 1.897 0 0 0-1.858-1.84 1.891 1.891 0 0 0-1.858 1.84v.807c0 .455-.371.82-.824.82a.826.826 0 0 1-.827-.82V6.507c0-1.161.947-2.106 2.093-2.106.259 0 .516.034.764.097a4.854 4.854 0 0 1 .352 9.713c-1.608.463-3.254.064-4.575-1.148a4.898 4.898 0 0 1-1.252-3.438v-2.07c0-2.798 2.292-5.087 5.092-5.087.46 0 .933.048 1.407.138a4.889 4.889 0 0 1 3.428 2.094c.452.554.706 1.244.706 1.982v6.03a4.867 4.867 0 0 1-1.268 3.334c-1.016 1.085-2.322 1.761-3.739 1.761a1.572 1.572 0 0 1-1.572-1.572v-5.83a1.572 1.572 0 0 1 1.572-1.572 1.578 1.578 0 0 1 1.572 1.572v1.396c0 .764.51 1.41 1.287 1.657a3.033 3.033 0 0 0 2.068.244c1.032-.246 1.868-1.106 2.213-2.156.04-.116.058-.242.058-.369v-5.31c0-.222-.066-.436-.186-.621-.22-.351-.577-.532-.968-.532a.832.832 0 0 0-.707.409.873.873 0 0 0-.103.514v1.07c0 .635-.516 1.145-1.149 1.145a1.145 1.145 0 0 1-1.146-1.145v-.538c0-2.63 2.143-4.773 4.773-4.773a4.793 4.793 0 0 1 2.204.56Z" clip-rule="evenodd"/>
-                    </svg>
-                    <span class="sr-only">TikTok page</span>
-                </a>
-
-                <a href="https://www.instagram.com/yourprofile" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M12.258 7.742a3.32 3.32 0 1 0 0 6.638 3.32 3.32 0 0 0 0-6.638ZM13.618 7.16a1.618 1.618 0 1 0 0 3.236 1.618 1.618 0 0 0 0-3.236ZM10 2h-2c-1.104 0-2 .896-2 2v14c0 1.104.896 2 2 2h12c1.104 0 2-.896 2-2V4c0-1.104-.896-2-2-2H10Zm-4 2h4v2H6V4Zm10 0v2h-4V4h4Zm-1 0h-2v2h2V4Zm-5 14H6v-2h4v2Zm8 0h-6v-2h6v2Zm0-4h-6v-2h6v2Zm2-4h-8v-2h8v2Zm0-4h-8V4h8v2Zm0 0H12V4h4v2Z" clip-rule="evenodd"/>
-                    </svg>
-                    <span class="sr-only">Instagram page</span>
-                </a>
-            </div>
+        <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div>
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+              <li class="mb-4">
+                <a href="https://www.instagram.com/yourprofile" class="hover:underline">Instagram</a>
+              </li>
+              <li>
+                <a href="https://www.tiktok.com/@yourprofile" class="hover:underline">TikTok</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+              <li class="mb-4">
+                <a href="#" class="hover:underline">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://yourwebsite.com/" class="hover:underline">Cinema app™</a>. All Rights Reserved.</span>
+        <div class="flex mt-4 sm:justify-center sm:mt-0 space-x-4">
+          <a href="https://www.facebook.com/yourprofile" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <img src="@/assets/img/instagram-1-svgrepo-com.svg" class="h-5" alt="Instagram Logo" />
+            <span class="sr-only">Facebook page</span>
+          </a>
+          <a href="https://www.tiktok.com/@yourprofile" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <img src="@/assets/img/tiktok-icon-white-1-logo-svgrepo-com.svg" class="h-5" alt="TikTok Logo" />
+            <span class="sr-only">TikTok page</span>
+          </a>
+          <a href="https://www.tiktok.com/@yourprofile" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <img src="@/assets/img/snapchat-snap-chat-svgrepo-com.svg" class="h-5" alt="Snap Logo" />
+            <span class="sr-only">Snap page</span>
+          </a>
+        </div>
+      </div>
     </div>
-</footer>
-
+  </footer>
 </template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+};
+</script>
+
+<style scoped>
+/* You can add custom styles for the footer here if needed */
+</style>
