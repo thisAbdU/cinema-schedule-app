87ecce53eb20c07a88fb90b99619ef7222bca64c
Abdu <abdulfetaheishak@gmail.com>
2024-09-20 18:07:37 +0300

    add movie thumbnail

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <MoviesAdBanner />
+    <MovieDetailsThumbnail />
   </div>
 </template>
---
+++ frontend/components/MovieDetails/Thumbnail.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex justify-center items-center bg-gray-100 rounded-lg shadow-lg w-full max-w-5xl mx-auto">
+      <!-- Thumbnail Image -->
+      <div class="flex-shrink-0 w-full h-64"> 
+        <img
+          :src="thumbnail"
+          alt="Movie Thumbnail"
+          class="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+    </div>
+  </template>
+  
+  //TODO: replace the thumbnail with the real movie thumbnail
+  <script>
+  export default {
+    data() {
+      return {
+        thumbnail: 'https://w0.peakpx.com/wallpaper/875/148/HD-wallpaper-movie-arrival-amy-adams-jeremy-renner.jpg', // Example thumbnail
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
